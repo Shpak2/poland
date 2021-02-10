701004bb473bdb603995c433584e42927747ea07
jack <jack@striking.email>
2021-02-10 23:51:49 +0200

    add hover & scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,43 @@
 'use strict'
 
 $( document ).ready(function() {
-  let header = $('#header-js');
+  let btnToTop = $('#scroll'),
+      header = $('#header-js');
 
+//scroll to bottom & function scroll to top
   $(window).scroll(function() {
     if($(window).scrollTop()  > 300) {
-      header.addClass('sticky')
+      header.addClass('sticky');
+      btnToTop.fadeIn(100);
     }else{
-      header.removeClass('sticky')
+      header.removeClass('sticky');
+      btnToTop.fadeOut(100);
     }
+  });
+
+  btnToTop.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, 700);
+  });
+// end scroll
+
+// img svg convert inline
+$('img.img-svg').each(function(){
+  var $img = $(this);
+  var imgClass = $img.attr('class');
+  var imgURL = $img.attr('src');
+  $.get(imgURL, function(data) {
+    var $svg = $(data).find('svg');
+    if(typeof imgClass !== 'undefined') {
+      $svg = $svg.attr('class', imgClass+' replaced-svg');
+    }
+    $svg = $svg.removeAttr('xmlns:a');
+    if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+    }
+    $img.replaceWith($svg);
+  }, 'xml');
 });
+// end img svg
+
 });
