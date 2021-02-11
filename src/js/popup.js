5d878c1194a6b058b89184aee1eaed6924c47a83
Alex Kirichenko <alexkirichenko@MacBook-Air-Alex.local>
2021-02-11 13:16:55 +0200

    change design btn

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
 
+    // ОТКРЫТЬ И ЗАКРЫТЬ ПОПАП
     let openPopup = $('.openPopup');
     let closePopup = $('.popupWrapper .modal__title_button');
     let popUp = $('.popupWrapper');
@@ -11,4 +12,16 @@ $( document ).ready(function() {
         $(popUp).hide();
         $('body').css('overflow', 'initial');
     })
+    // ОТКРЫТЬ И ЗАКРЫТЬ ПОПАП КОНЕЦ
+
+    //ПЕРЕКЛЮЧЕНИЕ АКТИВНОГО ДИЗАЙНА В ПОПАПЕ
+
+    let allDesignBtn = $('.designWrapper__item');
+    $(allDesignBtn).on('click', (e)=>{
+        $(allDesignBtn).removeClass('active');
+        $(e.target).closest('.designWrapper__item').addClass('active');
+    })
+
+    //ПЕРЕКЛЮЧЕНИЕ АКТИВНОГО ДИЗАЙНА В ПОПАПЕ КОНЕЦ
+
 })
